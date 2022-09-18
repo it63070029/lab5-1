bea404562e9388bfe990d7990f18226ddfd21fb2
it63070029 <63070029@it.kmitl.ac.th>
2022-09-19 02:02:24 +0700

    update

--- src/main/java/com/example/lab51/SentenceConsumer.java
+++ src/main/java/com/example/lab51/SentenceConsumer.java
@@ -6,16 +6,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class SentenceConsumer {
     protected Sentence sentences = new Sentence();
-    @RabbitListener(queues = "bad")
+    @RabbitListener(queues = "BadWordQueue")
     public void addBadSentence(String s){
         sentences.badSentences.add(s);
+        String text="";
+        for (int i=0; i<sentences.badSentences.size(); i++){
+            if (i>=1){
+                text = text+" , ";
+            }
+            text = text+""+sentences.badSentences.get(i);
 
-        System.out.println("In addBadSentence Method : ["+s+"]");
+        }
+
+        System.out.println("In addBadSentence Method : ["+text+"]");
     }
-    @RabbitListener(queues = "good")
+    @RabbitListener(queues = "GoodWordQueue")
     public void addGoodSentence(String s){
        sentences.goodSentences.add(s);
-        System.out.println("In addGoodSentence Method : ["+s+"]");
+       String text="";
+        for (int j=0; j<sentences.goodSentences.size(); j++){
+            if (j>=1){
+                text = text+",";
+            }
+            text = text+""+sentences.goodSentences.get(j);
+
+        }
+        System.out.println("In addGoodSentence Method : ["+text+"]");
 
     }
 
